04d42da347d2be52f0ddd0cda4ac499f7100ccdd
adricosmicfox <adriiphone@gmail.com>
2022-10-06 13:06:22 +0200

    crrated cards in coutrsescomponent with text and photos

--- src/components/CoursesComponent.vue
+++ src/components/CoursesComponent.vue
@@ -7,19 +7,94 @@
       </div>
       
 
-      <div>
+      <div class="d-flex flex-wrap">
 
-        <div class="am-card d-flex flex-column">
+        <div class="am-card d-flex flex-column justify-content-between">
           <img class="am-photo" src="../assets/course-02-480x298.jpg" alt="">
           <div class="d-flex flex-column px-4">
-            <div class="d-flex"> 
+            <div class="d-flex align-items-center"> 
               <img class="am-little-avatar" src="../assets/73ee246daf47502812ccefc84bf02898 (1).jpeg" alt="">
-              <p>Blanche Fields</p>
+              <p class="m-0 ms-2">Blanche Fields</p>
             </div>
-            <h5>Learning to Write as a Professional Author</h5>
+            <h5 class="mt-2">Learning to Write as a Professional Author</h5>
             <div class="d-flex"> 
               <p>20 Lessons</p>
-              <p>50 Students</p>
+              <p class="ms-3">50 Students</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="am-card d-flex flex-column justify-content-between">
+          <img class="am-photo" src="../assets/stock-full-hd-03-480x298.jpg" alt="">
+          <div class="d-flex flex-column px-4">
+            <div class="d-flex align-items-center"> 
+              <img class="am-little-avatar" src="../assets/d0d504142acfde820eef2f11feea6253 (1).jpeg" alt="">
+              <p class="m-0 ms-2">Maggie Strickland</p>
+            </div>
+            <h5 class="mt-2">Customer-centric Info-Tech Strategies</h5>
+            <div class="d-flex"> 
+              <p>24 Lessons</p>
+              <p class="ms-3">769 Students</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="am-card d-flex flex-column justify-content-between">
+          <img class="am-photo" src="../assets/stock-full-hd-04-480x298.jpg" alt="">
+          <div class="d-flex flex-column px-4">
+            <div class="d-flex align-items-center"> 
+              <img class="am-little-avatar" src="../assets/d0d504142acfde820eef2f11feea6253 (1).jpeg" alt="">
+              <p class="m-0 ms-2">Maggie Strickland</p>
+            </div>
+            <h5 class="mt-2">Learning to Write as a Professional Author</h5>
+            <div class="d-flex"> 
+              <p>20 Lessons</p>
+              <p class="ms-3">50 Students</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="am-card d-flex flex-column justify-content-between">
+          <img class="am-photo" src="../assets/stock-full-hd-06-480x298.jpg" alt="">
+          <div class="d-flex flex-column px-4">
+            <div class="d-flex align-items-center"> 
+              <img class="am-little-avatar" src="../assets/73ee246daf47502812ccefc84bf02898 (1).jpeg" alt="">
+              <p class="m-0 ms-2">Blanche Fields</p>
+            </div>
+            <h5 class="mt-2">Accademic Listening and Note-taking</h5>
+            <div class="d-flex"> 
+              <p>14 Lessons</p>
+              <p class="ms-3">67 Students</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="am-card d-flex flex-column justify-content-between">
+          <img class="am-photo" src="../assets/course-featured-image-01-480x298.jpg" alt="">
+          <div class="d-flex flex-column px-4">
+            <div class="d-flex align-items-center"> 
+              <img class="am-little-avatar" src="../assets/73ee246daf47502812ccefc84bf02898 (1).jpeg" alt="">
+              <p class="m-0 ms-2">Blanche Fields</p>
+            </div>
+            <h5 class="mt-2">Master jQuery in a Short Period of Time</h5>
+            <div class="d-flex"> 
+              <p>6 Lessons</p>
+              <p class="ms-3">51 Students</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="am-card d-flex flex-column justify-content-between">
+          <img class="am-photo" src="../assets/stock-full-hd-05-480x298.jpg" alt="">
+          <div class="d-flex flex-column px-4">
+            <div class="d-flex align-items-center"> 
+              <img class="am-little-avatar" src="../assets/73ee246daf47502812ccefc84bf02898 (1).jpeg" alt="">
+              <p class="m-0 ms-2">Blanche Fields</p>
+            </div>
+            <h5 class="mt-2">Introduction to Javascript for Beginners</h5>
+            <div class="d-flex"> 
+              <p>14 Lessons</p>
+              <p class="ms-3">76 Students</p>
             </div>
           </div>
         </div>
@@ -49,7 +124,9 @@ section{
 }
 
 .am-card{
-  width: 25rem;
+  width: 24rem;
+  height: 26rem;
+  padding: 1rem;
 }
 
 .am-photo{
