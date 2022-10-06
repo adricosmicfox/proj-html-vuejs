792c79fd6eaa2645c26baa9c6cef29c83d81b1ca
adricosmicfox <adriiphone@gmail.com>
2022-10-06 16:22:18 +0200

    created events cards in eventscoponents

--- src/components/EventsComponent.vue
+++ src/components/EventsComponent.vue
@@ -1,9 +1,105 @@
 <template>
   <section>
-     <div></div>
-  </section>
- 
-  
+     <div class="d-flex justify-content-around align-items-center">
+        <div class="d-flex flex-column align-items-center">
+          <h6>GET IN CONTACT NOW</h6>
+          <h1>Upcoming Events</h1>
+        </div>
+      </div>
+
+      
+      <div class="d-flex justify-content-between flex-wrap">
+        <!-- ***EVENT*** -->
+        <div class="am-event d-flex p-4 justify-content-between">
+          <div class="d-flex flex-column align-self-center"> 
+            <h6>Texas,US</h6>
+            <h5>Storytelling Workshop</h5>
+          </div>
+         
+          <div class="d-flex flex-column align-items-center">
+            <h1>22</h1>
+            <h5>NOV</h5>
+            <button> Get ticket</button>
+          </div>
+        </div>
+
+        <!-- ***EVENT*** -->
+        <div class="am-event d-flex p-4 justify-content-between">
+          <div class="d-flex flex-column align-self-center"> 
+            <h6>Texas,US</h6>
+            <h5>Storytelling Workshop</h5>
+          </div>
+         
+          <div class="d-flex flex-column align-items-center">
+            <h1>22</h1>
+            <h5>NOV</h5>
+            <button> Get ticket</button>
+          </div>
+        </div>
+
+            <!-- ***EVENT*** -->
+            <div class="am-event d-flex p-4 justify-content-between">
+          <div class="d-flex flex-column align-self-center"> 
+            <h6>Texas,US</h6>
+            <h5>Storytelling Workshop</h5>
+          </div>
+         
+          <div class="d-flex flex-column align-items-center">
+            <h1>22</h1>
+            <h5>NOV</h5>
+            <button> Get ticket</button>
+          </div>
+        </div>
+
+        <!-- ***EVENT*** -->
+        <div class="am-event d-flex p-4 justify-content-between">
+          <div class="d-flex flex-column align-self-center"> 
+            <h6>Texas,US</h6>
+            <h5>Storytelling Workshop</h5>
+          </div>
+         
+          <div class="d-flex flex-column align-items-center">
+            <h1>22</h1>
+            <h5>NOV</h5>
+            <button> Get ticket</button>
+          </div>
+        </div>
+
+            <!-- ***EVENT*** -->
+            <div class="am-event d-flex p-4 justify-content-between">
+          <div class="d-flex flex-column align-self-center"> 
+            <h6>Texas,US</h6>
+            <h5>Storytelling Workshop</h5>
+          </div>
+         
+          <div class="d-flex flex-column align-items-center">
+            <h1>22</h1>
+            <h5>NOV</h5>
+            <button> Get ticket</button>
+          </div>
+        </div>
+
+        <!-- ***EVENT*** -->
+        <div class="am-event d-flex p-4 justify-content-between">
+          <div class="d-flex flex-column align-self-center"> 
+            <h6>Texas,US</h6>
+            <h5>Storytelling Workshop</h5>
+          </div>
+         
+          <div class="d-flex flex-column align-items-center">
+            <h1>22</h1>
+            <h5>NOV</h5>
+            <button> Get ticket</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="d-flex justify-content-center"> 
+        <h6>Excited about our event? View all events</h6>
+      </div>
+        
+    
+    </section>
 </template>
 
 
@@ -22,4 +118,21 @@ section{
     padding-left: 17rem;
     padding-right: 17rem;
 }
+
+.am-event{
+  margin-top: 2rem;
+  width: 37.4rem;
+  background-color: #F8F8F8;
+  border-radius: 4px;
+}
+
+button{
+  color: white;
+  border: none;
+  border-radius: 7px;
+  padding: .3rem;
+  width: 10rem;
+  font-weight: 600;
+}
+
 </style>
