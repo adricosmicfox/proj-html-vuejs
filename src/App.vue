1c20789a8b2ab50bf4b8ba87875ffe05cc659bed
adricosmicfox <adriiphone@gmail.com>
2022-10-10 14:55:37 +0200

    added wave to jumbotron

--- src/App.vue
+++ src/App.vue
@@ -38,14 +38,22 @@ export default {
 </script>
 
 <style lang="scss">
+
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,400;0,500;0,700;0,800;0,900;1,300;1,400;1,500;1,700;1,800;1,900&display=swap');
+
 #app {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
+  font-family: 'Poppins', sans-serif;
     }
 
 section{
-  padding-top: 3rem;
-  padding-bottom: 3rem;
+  padding-top: 2rem;
+  padding-bottom: 2rem;
+}
+
+h1{
+  font-weight: 600 !important;
 }
 </style>
