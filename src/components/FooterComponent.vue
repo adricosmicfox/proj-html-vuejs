35eedec3eb533457a1a385d5f40c8e122cabb02c
adricosmicfox <adriiphone@gmail.com>
2022-10-10 11:59:59 +0200

    added content in footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,7 +1,42 @@
 <template>
-  <div></div>
-  
-</template>
+  <section> 
+
+    <div class="d-flex justify-content-between">
+      <div class="d-flex flex-column">
+        <h5>Address</h5>
+        <p>382 NE 191st St #87394 Miami, FL 33179-3899</p>
+        <p>+1(305) 547-9909 (9am - 5pm EST, Monday - Friday)</p>
+        <p>support@maxcoach.com</p>
+
+      </div>
+
+
+      <div class="am-informations d-flex justify-content-between">
+        <div class="d-flex flex-column">
+          <h5>Explore</h5>
+          <p>Start here</p>
+          <p>Blog</p>
+          <p>About us</p>
+        </div>
+
+        <div class="d-flex flex-column">
+          <h5>&nbsp;</h5>
+          <p>Success story</p>
+          <p>Courses</p>
+          <p>Contact us</p>
+        </div>
+
+        <div class="d-flex flex-column">
+          <h5>Informations</h5>
+          <p>Membership</p>
+          <p>Purchase guida</p>
+          <p>Privacy policy</p>
+          <p>Terms of services</p>
+        </div>
+      </div>
+    </div>
+  </section>
+ </template>
 
 
 <!-- *****JAVSCRIPT***** -->
@@ -15,5 +50,12 @@ name: 'FooterComponent'
 
 <!-- *****CSS STYLE***** -->
 <style lang="scss" scoped>
+section{
+    padding-left: 17rem;
+    padding-right: 17rem;
+}
+.am-informations{
+  width: 30rem;
+}
 
 </style>
